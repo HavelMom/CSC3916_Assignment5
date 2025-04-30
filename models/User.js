98be2bc413642745876b913d588a771fdc6a0cf3
Alan Delgado <odagled@Conquest.local>
2025-04-30 16:13:23 -0600

    Modified react repo

--- models/User.js
+++ models/User.js
@@ -20,21 +20,6 @@ var UserSchema = new Schema({
     password: { type: String, required: true, select: false }
 });
 
-UserSchema.pre('save', function(next) {
-    var user = this;
-
-    //hash the password
-    if (!user.isModified('password')) return next();
-
-    bcrypt.hash(user.password, null, null, function(err, hash) {
-        if (err) return next(err);
-
-        //change the password
-        user.password = hash;
-        next();
-    });
-});
-
 UserSchema.methods.comparePassword = function (password, callback) {
     var user = this;
 
